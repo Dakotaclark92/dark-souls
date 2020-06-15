ce0bbfb7d8973df770b6390f1ef40208fb8509b0
Dakota <Dakotaclark92@yahoo.com>
2020-06-15 12:26:37 -0500

    testing git status

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+    def new
+        @users = User.all
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:notice] = "The Bell has not Tolled"
+            render :new
+        end
+    end
+
+    def sigh_out
+        session.clear
+        redirect_to root_path
+    end
+    
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'about', to: 'static#about'
+
+  root "home#index"
+  resources :users
+  
+  get '/signin' => "sessions#new"
+  post '/signin' => "sessions#create"
+  delete '/signout' => "sessions#sign_out"
+
+
+
 
 end
