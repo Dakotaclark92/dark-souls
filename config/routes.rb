fc18c8b0c9263982cc59aaf5ef0bb5aacadfd1b3
Dakota <Dakotaclark92@yahoo.com>
2020-06-23 12:44:31 -0500

    u

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "home#index"
-  resources :users
+  resources :user
   
-  get '/users/new' => "user#new"
-  post '/users/new' => "user#create"
+  get '/user/new' => "user#new"
+  post '/user/new' => "user#create"
   delete '/signout' => "sessions#sign_out"
 
 
